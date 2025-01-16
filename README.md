ed90d8a4c02b80ca145448764174ca5615370a77
Rafael <rafael.sergio@nextpage.com.br>
2025-01-15 21:07:50 -0300

    feat: validate distance of check in

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ GymPass Style App
 ## RNs
 
 - [x] The user must not be able to register with a duplicate email;
-- [] The user cannot make 2 check-ins on the same day;
+- [x] The user cannot make 2 check-ins on the same day;
 - [] The user cannot check-in if he/she is not close (100m) to the gym
 - [] The check-in can only be validated up to 20 minutes after being created;
 - [] The check-in can only be validated by administrators
--- src/repositories/in-memory/in-memory-check-ins.repository.ts
+++ src/repositories/in-memory/in-memory-check-ins.repository.ts
@@ -1,10 +1,29 @@
 import { Prisma, type CheckIn } from '@prisma/client'
-
+import dayjs from 'dayjs'
 import { CheckInsRepository } from '../check-ins-repository'
 import { randomUUID } from 'node:crypto'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public itens: CheckIn[] = []
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkInOnSameDate = this.itens.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at)
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+      return checkIn.user_id === userId && isOnSameDate
+    })
+
+    if (!checkInOnSameDate) {
+      return null
+    }
+
+    return checkInOnSameDate
+  }
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
